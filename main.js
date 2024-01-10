99f1f49e5350eea4055494da428dfc8f65ff286b
HumbertoMachado7 <humbertomachado777@gmail.com>
2024-01-10 18:25:40 -0300

    funcionando la movilidad de la figura y soldifica bien, como tembien elimina la linea completa

--- main.js
+++ main.js
@@ -56,8 +56,34 @@ const piece = {
     ]
 }
 
-//2. game loop
-function update() {
+// //2. game loop
+// function update() {
+//     draw()
+//     window.requestAnimationFrame(update)
+// }
+
+// 8. auto drop
+let dropCounter = 0
+let lastTime = 0
+
+function update(time=0) {
+    const deltaTime = time - lastTime
+    lastTime = time
+
+dropCounter += deltaTime
+
+if (dropCounter > 1000 ){
+    piece.position.y++
+    dropCounter = 0
+
+    if (checkCollision()){
+        piece.position.y--
+        solidifyPiece()
+        removeRows()
+    }
+}
+
+
     draw()
     window.requestAnimationFrame(update)
 }
